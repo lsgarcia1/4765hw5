6b872d7ef6f2df794e3a478da9a0e10497e3f139
lsgarcia1 <lgarci20@uwyo.edu>
2020-09-27 14:12:01 -0600

    Parts 1 and 2 Done

--- 4765hw5.cpp
+++ 4765hw5.cpp
@@ -1,20 +1,28 @@
-// 4765hw5.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <string>
+
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	string input;
+	cout << "Enter a string" << endl;
+    getline(cin, input);
+	
+	for (int i = 0, len = input.size(); i < len; i++)
+	{
+		if (ispunct(input[i]) || isspace(input[i]))
+		{
+			input.erase(i--, 1);
+			len = input.size();
+		}
+		else 
+		{
+			input[i] = tolower(input[i]);
+		}
+		
+	}
+
+	cout << input;
+	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
